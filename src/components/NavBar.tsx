498b7c28e47ca6dc02f33d20732741240c50ac61
Dmitry <dimaandreev1337@gmail.com>
2023-02-19 21:14:41 +0700

    add burger menu

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,6 +4,7 @@ import Container from "./Container";
 import Logo from '../assets/images/logo.png'
 
 const NavBar = () => {
+
   return (
     <nav className="navBar">
       <Container>
@@ -17,7 +18,23 @@ const NavBar = () => {
             <p className="navBar__items__btn">портфолио</p>
             <p className="navBar__items__btn">услуги и цены</p>
             <p className="navBar__items__btn">контакты</p>
-            <p style={{ cursor: 'default' }} className="navBar__items__btn">{juliaPhoneNumber}</p>
+            <p className="navBar__items__phone">{juliaPhoneNumber}</p>
+          </div>
+
+          <div className="burger-menu">
+            <input id="menu-toggle" type="checkbox" />
+            <label className="menu-btn" htmlFor="menu-toggle">
+              <span />
+            </label>
+            <ul className="menubox">
+              <div>
+                <p className="burger-menu__item">о нас</p>
+                <p className="burger-menu__item">портфолио</p>
+                <p className="burger-menu__item">услуги и цены</p>
+                <p className="burger-menu__item">контакты</p>
+                <p className="burger-menu__phone">{juliaPhoneNumber}</p>
+              </div>
+            </ul>
           </div>
         </div>
       </Container>
