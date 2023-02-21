07fc79dd78f993bc816525596f77cb1dc246ea81
Dmitry <dimaandreev1337@gmail.com>
2023-02-21 22:01:33 +0700

    add services cards

---
+++ src/components/ServicesCard.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { servicesCard } from '../common/utils'
+import Background from '../assets/images/backgrounds/contactsBackground.png'
+import Arrow from '../assets/images/icons/blueLeftArrow.svg'
+
+interface IServicesCardProps {
+  cardId: number
+  closeCard: () => void
+}
+
+const ServicesCard = (props: IServicesCardProps) => {
+  return (
+    <div
+      style={{ backgroundImage: `url(${Background})` }}
+      className='services-card'>
+      {servicesCard.map((item, index) => {
+        if (item.id === props.cardId) {
+          return (
+            <div key={index} className='services-card__content'>
+              <div style={{ width: '50%' }}>
+                {item.features.map((item, index) => {
+                  return (
+                    <div key={index} className='services-card__features'>
+                      <p>{item}</p>
+                    </div>
+                  )
+                })}
+              </div>
+              <div style={{ width: '30%' }} key={index}>
+                {item.cost.map((item, index) => {
+                  return (
+                    <div className='services-card__cost' key={index}>
+                      <p>{item}</p>
+                    </div>
+                  )
+                })}
+              </div>
+            </div>
+          )
+        }
+      })}
+      <div onClick={props.closeCard} className="services-card__close-arrow">
+        <img src={Arrow} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default ServicesCard
--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -1,49 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import BackgroundLeft from '../assets/images/backgrounds/contactsBackground.png'
 import BackgroundRight from '../assets/images/backgrounds/meetingBackground.png'
 import { meeting, services } from "../common/utils";
-import Arrow from '../assets/images/icons/arrow.svg'
+import Arrow from '../assets/images/icons/blueRightArrow.svg'
+import ServicesCard from "../components/ServicesCard";
 
 const Services = () => {
+
+  const [cardId, setCardId] = useState(0)
+  const [showCard, setShowCard] = useState(false)
+
   return (
-    <section className="services-and-price">
-      <div style={{ backgroundImage: `url(${BackgroundLeft})` }} className="services">
-        <p className="services__title">услуги и цены</p>
-        <div className="services__services-wrapper">
-          {services.map((item, index) => {
-            return (
-              <div key={index} className="services__element">
-                <p className="services__subtitle">{item.subtitle}</p>
-                <p className="services__text">{item.text}</p>
-                <div className="services__arrow">
-                  <img src={Arrow} alt="" />
+    <>
+      {showCard
+        &&
+        <ServicesCard
+          closeCard={() => { setShowCard(false) }}
+          cardId={cardId} />
+      }
+      <section className="services-and-price">
+        <div style={{ backgroundImage: `url(${BackgroundLeft})` }} className="services">
+          <p className="services__title">услуги и цены</p>
+          <div className="services__services-wrapper">
+            {services.map((item, index) => {
+              return (
+                <div key={index} className="services__element">
+                  <p className="services__subtitle">{item.subtitle}</p>
+                  <p className="services__text">{item.text}</p>
+                  <div onClick={() => {
+                    setCardId(item.id)
+                    setShowCard(true)
+                  }} className="services__arrow">
+                    <img src={Arrow} alt="" />
+                  </div>
+                  {item.cost.map((item, index) => {
+                    return (
+                      <div key={index}>
+                        <p className="services__cost-mobile">{item}</p>
+                      </div>
+                    )
+                  })}
                 </div>
-                {item.cost.map((item, index) => {
-                  return (
-                    <div key={index}>
-                      <p className="services__cost-mobile">{item}</p>
-                    </div>
-                  )
-                })}
-              </div>
-            )
-          })}
+              )
+            })}
+          </div>
         </div>
-      </div>
-      <div style={{ backgroundImage: `url(${BackgroundRight})` }} className="meeting">
-        <p className="meeting__title">C чего начать с нами работу</p>
-        <div className="meeting__opacity">
-          {meeting.map((item, index) => {
-            return (
-              <div key={index} className="meeting__element">
-                <p className="meeting__subtitle">{item.subtitle}</p>
-                <p className="meeting__text">{item.text}</p>
-              </div>
-            )
-          })}
+        <div style={{ backgroundImage: `url(${BackgroundRight})` }} className="meeting">
+          <p className="meeting__title">C чего начать с нами работу</p>
+          <div className="meeting__opacity">
+            {meeting.map((item, index) => {
+              return (
+                <div key={index} className="meeting__element">
+                  <p className="meeting__subtitle">{item.subtitle}</p>
+                  <p className="meeting__text">{item.text}</p>
+                </div>
+              )
+            })}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   )
 }
 
