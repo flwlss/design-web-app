2d9ce5ba1b1f936607c6b71712b05cb168144c0d
Dmitry <dimaandreev1337@gmail.com>
2023-03-18 15:22:12 +0700

    remove feedback card

--- src/components/HeaderButton.tsx
+++ src/components/HeaderButton.tsx
@@ -1,19 +1,11 @@
 import React, { useState } from "react";
-import FeedbackCard from "./FeedbackCard";
 
 const HeaderButton = () => {
 
-  const [state, setState] = useState(false)
-
-  const ButtonAction = () => {
-    setState(true)
-  }
-  
   return (
     <>
-      {state && <FeedbackCard />}
       <button
-        onClick={ButtonAction}
+        onClick={() => {}}
         className="header__button">
         Заказать проект
       </button>
