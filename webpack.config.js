79ca9c986c4a36c22741c9cdad2c9cc1d3d31ffe
Dmitry <dimaandreev1337@gmail.com>
2023-03-20 13:28:02 +0700

    added image webpack loader

--- webpack.config.js
+++ webpack.config.js
@@ -50,10 +50,37 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|jp2|webp|css|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-        },
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          },
+        ],
         exclude: /node_modules/,
       },
     ]
