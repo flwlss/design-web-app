ac9c4356f7078e210b1e250f7369f835d1108f08
Dmitry <dimaandreev1337@gmail.com>
2023-03-22 20:58:00 +0700

    favicon works

--- webpack.config.js
+++ webpack.config.js
@@ -94,6 +94,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: 'index.html',
+      favicon: './public/favicon.ico',
     }),
   ],
   devServer: {
