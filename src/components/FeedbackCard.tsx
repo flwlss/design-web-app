dc53b1cdf22b4edb5370f47f5745ffbf8fc5c515
Dmitry <dimaandreev1337@gmail.com>
2023-03-27 17:21:42 +0700

    added spinner after send form

--- src/components/FeedbackCard.tsx
+++ src/components/FeedbackCard.tsx
@@ -1,5 +1,5 @@
 import { useFormik } from "formik";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import LeftBackground from '../assets/images/backgrounds/leftFormBackground.jpg'
 import RightBackground from '../assets/images/backgrounds/rightFormBackground.jpg'
 import ContactsButton from "./ContactsButton";
@@ -7,12 +7,15 @@ import * as Yup from 'yup';
 import IMask from 'imask';
 import emailjs from '@emailjs/browser';
 import { toast } from 'react-toastify';
+import Spinner from "./Spinner";
 
 interface IFeedbackCardProps {
   closeAction: () => void
 }
 
 const FeedbackCard = (props: IFeedbackCardProps) => {
+
+  const [showSpinner, setShowSpinner] = useState(false)
 
   useEffect(() => {
     IMask(
@@ -32,10 +35,12 @@ const FeedbackCard = (props: IFeedbackCardProps) => {
     }),
     onSubmit: values => {
       if (values) {
+        setShowSpinner(true)
         emailjs.sendForm('service_5vyztfl', 'template_0i0s8wb', '#feedbackForm', 'Sou9g0OIZpFtqc25d')
           .then((result) => {
             props.closeAction()
             toast.success('Заявка успешно отправлена')
+            setShowSpinner(false)
           }, (error) => {
             toast.error('Ошибка')
           });
@@ -44,64 +49,67 @@ const FeedbackCard = (props: IFeedbackCardProps) => {
   });
 
   return (
-    <form id="feedbackForm" onSubmit={formik.handleSubmit} className="feedbackCard">
-      <div
-        onClick={props.closeAction}
-        style={{ backgroundImage: `url(${LeftBackground})` }}
-        className="feedbackCard__closeCard" />
-      <div
-        style={{ backgroundImage: `url(${LeftBackground})` }}
-        className="feedbackCard__left">
-        <p className="feedbackCard__left__title">{`заказать\nобратный звонок`}</p>
-        <ContactsButton
-          width="100%"
-          title="Отправить" />
-      </div>
-      <div
-        style={{ backgroundImage: `url(${RightBackground})` }}
-        className="feedbackCard__right">
-        <div className="feedbackCard__right__inputWrapper">
-          <label
-            className='feedbackCard__right__label'
-            htmlFor="name">Ваше имя</label>
-          <input
-            className='feedbackCard__right__input'
-            id="name"
-            name="name"
-            placeholder='Иван'
-            type="text"
-            onChange={formik.handleChange}
-            value={formik.values.name.replace(/[^а-яёА-ЯЁ]/g, '')}
-          />
-          {formik.touched.name && formik.errors.name ? (
-            <div className='feedbackCard__error'>{formik.errors.name}</div>
-          ) : null}
-        </div>
-        <div className="feedbackCard__right__inputWrapper">
-          <label
-            className='feedbackCard__right__label'
-            htmlFor="phone">Телефон</label>
-          <input
-            className='feedbackCard__right__input'
-            id="phone"
-            name="phone"
-            placeholder='+7(000)000-00-00'
-            type="tel"
-            onChange={formik.handleChange}
-            value={formik.values.phone}
-          />
-          {formik.touched.phone && formik.errors.phone ? (
-            <div className='feedbackCard__error'>{formik.errors.phone}</div>
-          ) : null}
-        </div>
-        <p className='feedbackCard__right__text'>{`мы вам перезвоним\nчерез 5 минут`}</p>
-        <div className='feedbackCard__secondButton'>
+    <>
+      {showSpinner && <Spinner />}
+      <form id="feedbackForm" onSubmit={formik.handleSubmit} className="feedbackCard">
+        <div
+          onClick={props.closeAction}
+          style={{ backgroundImage: `url(${LeftBackground})` }}
+          className="feedbackCard__closeCard" />
+        <div
+          style={{ backgroundImage: `url(${LeftBackground})` }}
+          className="feedbackCard__left">
+          <p className="feedbackCard__left__title">{`заказать\nобратный звонок`}</p>
           <ContactsButton
             width="100%"
             title="Отправить" />
         </div>
-      </div>
-    </form>
+        <div
+          style={{ backgroundImage: `url(${RightBackground})` }}
+          className="feedbackCard__right">
+          <div className="feedbackCard__right__inputWrapper">
+            <label
+              className='feedbackCard__right__label'
+              htmlFor="name">Ваше имя</label>
+            <input
+              className='feedbackCard__right__input'
+              id="name"
+              name="name"
+              placeholder='Иван'
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.name.replace(/[^а-яёА-ЯЁ]/g, '')}
+            />
+            {formik.touched.name && formik.errors.name ? (
+              <div className='feedbackCard__error'>{formik.errors.name}</div>
+            ) : null}
+          </div>
+          <div className="feedbackCard__right__inputWrapper">
+            <label
+              className='feedbackCard__right__label'
+              htmlFor="phone">Телефон</label>
+            <input
+              className='feedbackCard__right__input'
+              id="phone"
+              name="phone"
+              placeholder='+7(000)000-00-00'
+              type="tel"
+              onChange={formik.handleChange}
+              value={formik.values.phone}
+            />
+            {formik.touched.phone && formik.errors.phone ? (
+              <div className='feedbackCard__error'>{formik.errors.phone}</div>
+            ) : null}
+          </div>
+          <p className='feedbackCard__right__text'>{`мы вам перезвоним\nчерез 5 минут`}</p>
+          <div className='feedbackCard__secondButton'>
+            <ContactsButton
+              width="100%"
+              title="Отправить" />
+          </div>
+        </div>
+      </form>
+    </>
   )
 }
 
---
+++ src/components/Spinner.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { RotatingLines } from "react-loader-spinner";
+
+const Spinner = () => {
+  return (
+    <div className="spinner">
+      <RotatingLines
+        strokeColor="rgb(20,24,33)"
+        strokeWidth="5"
+        animationDuration="0.75"
+        width="96"
+        visible={true}
+      />
+    </div>
+  )
+}
+
+export default Spinner
